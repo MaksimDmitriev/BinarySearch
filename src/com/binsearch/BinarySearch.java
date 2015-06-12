c40f9311cd5d4715b783f41e5f3343f506d08fc9
Maksim Dmitriev <maksim.a.dmitriev@gmail.com>
2015-06-12 21:08:03 +0200

    Add an iterative method

--- src/com/binsearch/BinarySearch.java
+++ src/com/binsearch/BinarySearch.java
@@ -2,19 +2,20 @@ package com.binsearch;
 
 public class BinarySearch {
 
+    private static boolean isArrayValid(int[] array) {
+        return array != null && array.length > 0;
+    }
+
     public static boolean binarySearch(int[] array, int k) {
-        if (array == null || array.length == 0) {
-            return false;
-        }
-        return binarySearch(0, array.length - 1, k, array);
+        return isArrayValid(array) ? binarySearch(0, array.length - 1, k, array) : false;
     }
 
     private static boolean binarySearch(int left, int right, int k, int[] arr) {
-        int middleIndex = (left + right) / 2;
-        int middle = arr[middleIndex];
         if (left > right) {
             return false;
         }
+        int middleIndex = (left + right) / 2;
+        int middle = arr[middleIndex];
         if (middle == k) {
             return true;
         } else if (k < middle) {
@@ -22,6 +23,24 @@ public class BinarySearch {
         } else {
             return binarySearch(middleIndex + 1, right, k, arr);
         }
+    }
+
+    private static boolean binarySearchLoop(int left, int right, int k, int[] arr) {
+        while (left <= right) {
+            int middleIndex = (left + right) / 2;
+            if (k == arr[middleIndex]) {
+                return true;
+            } else if (k < arr[middleIndex]) {
+                right = middleIndex - 1;
+            } else {
+                left = middleIndex + 1;
+            }
+        }
+        return false;
+    }
+
+    public static boolean binarySearchLoop(int[] array, int k) {
+        return isArrayValid(array) ? binarySearchLoop(0, array.length - 1, k, array) : false;
     }
 
 }
--- src/com/binsearch/Main.java
+++ src/com/binsearch/Main.java
@@ -3,11 +3,12 @@ package com.binsearch;
 public class Main {
 
     public static void main(String[] args) {
-        int n = 100000;
+        int n = 1000000;
         int array[] = new int[n];
         for (int i = 0; i < n; i++) {
             array[i] = i;
         }
-        System.out.println(BinarySearch.binarySearch(array, 1));
+        System.out.println(BinarySearch.binarySearch(array, 0));
+        System.out.println(BinarySearch.binarySearchLoop(array, 0));
     }
 }
